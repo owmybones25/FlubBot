8836353efa648d18cd4ac28e106ca72eefd4749a
NicerNewerCar <30351234+NicerNewerCar@users.noreply.github.com>
2020-05-08 14:32:12 -0400

    Update Bot.py
    Moved Deep Dream API Call to it's own function

--- bot.py
+++ bot.py
@@ -11,61 +11,64 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-	if message.author == client.user:
-		return
-	num = random.randint(0,101)
-
-	if message.content.startswith('$flube'):
-		try:
-			if (message.attachments[0].id > 0):
-				dreamer(10)
-			else:
-				await message.channel.send('FLUB... Error... Fluuuub... Try again but with the command as a caption to a photo Fluuuuuuuuuuuuuuuuuuuubbbb!!!')
-		except IndexError:
-			pass
-
-	try:
-		if(message.attachments[0].id > 0) and num > 95:
-			dreamer(4)
-	except IndexError:
-		pass
-	if num >= 90:
-		x = random.randint(0,14)
-		if x == 0:
-			msg = 'flub Flub FLUB?'
-		elif x == 1:
-			msg = 'fLuUuUuB?!?!?!?!'
-		elif x == 2:
-			msg = 'flub flub flub flub flub flub?'
-		elif x == 3:
-			msg = 'FLUB!'
-		elif x == 4:
-			msg = 'FLuB FlUB fluwb FLUB'
-		elif x == 5:
-			msg = 'Flub'
-		elif x == 6:
-			msg = 'flubber'
-		elif x == 7:
-			msg = 'flubbed'
-		elif x == 8:
-			msg = 'F̡̡̧̤͕̯̱̗̝͖̫͚̞͙͙͖̻̘̙̅͑ͣ͛̈̀ͅl̵̢̢͙͖͓̰̱̗͕̻̯͈͍̰͈̣̻̿ͥ̈́͛̈̎̌̐͜͝ͅữ̸̡̢̲̬̲̲̠̝̣͎͌̃ͨ̿̉ͦ̇̏̿̇͌͌̋̆ͮͫͩ̐͘b̸͎̜̼̌͂͑̃̍ͣͧͦ̐͂ͭ̿̆ͤͤͧ͘͘͘͜'
-		elif x == 9:
-			msg = 'flubted'
-		elif x == 10:
-			msg = 'F,L,U,B,'
-		elif x == 11:
-			msg = 'Flub me daddy'
-		elif x == 12:
-			msg = 'FFFFFFFFFLLLLLLLLLLLLLUUUUUUUUUUUBBBBBBBBBBB'
-		elif x == 13:
-			msg = 'f       l           u         b'
-		else:
-			msg = 'F... L... U... B...'
-		await message.channel.send(msg)
+    if message.author == client.user:
+        return
+    num = random.randint(0,101)
+    if message.content.startswith('$flube'):
+        try:
+            if (message.attachments[0].id > 0):
+                url = message.attachments[0].url
+                url = dreamer(10,url)
+                await message.channel.send(url)
+                await message.channel.send('FℲ... L˥... U∩... Bq...')
+            else:
+                await message.channel.send('FLUB... Error... Fluuuub... Try again but with the command as a caption to a photo Fluuuuuuuuuuuuuuuuuuuubbbb!!!')
+        except IndexError:
+            pass
+    try:
+        if(message.attachments[0].id > 0) and num > 95:
+            url = message.attachments[0].url
+            url = dreamer(4,url)
+            await message.channel.send(url)
+            await message.channel.send('FℲ... L˥... U∩... Bq...')
+    except IndexError:
+        pass
+    if num >= 90:
+        x = random.randint(0,14)
+        if x == 0:
+            msg = 'flub Flub FLUB?'
+        elif x == 1:
+            msg = 'fLuUuUuB?!?!?!?!'
+        elif x == 2:
+            msg = 'flub flub flub flub flub flub?'
+        elif x == 3:
+            msg = 'FLUB!'
+        elif x == 4:
+            msg = 'FLuB FlUB fluwb FLUB'
+        elif x == 5:
+            msg = 'Flub'
+        elif x == 6:
+            msg = 'flubber'
+        elif x == 7:
+            msg = 'flubbed'
+        elif x == 8:
+            msg = 'F̡̡̧̤͕̯̱̗̝͖̫͚̞͙͙͖̻̘̙̅͑ͣ͛̈̀ͅl̵̢̢͙͖͓̰̱̗͕̻̯͈͍̰͈̣̻̿ͥ̈́͛̈̎̌̐͜͝ͅữ̸̡̢̲̬̲̲̠̝̣͎͌̃ͨ̿̉ͦ̇̏̿̇͌͌̋̆ͮͫͩ̐͘b̸͎̜̼̌͂͑̃̍ͣͧͦ̐͂ͭ̿̆ͤͤͧ͘͘͘͜'
+        elif x == 9:
+            msg = 'flubted'
+        elif x == 10:
+            msg = 'F,L,U,B,'
+        elif x == 11:
+            msg = 'Flub me daddy'
+        elif x == 12:
+            msg = 'FFFFFFFFFLLLLLLLLLLLLLUUUUUUUUUUUBBBBBBBBBBB'
+        elif x == 13:
+            msg = 'f       l           u         b'
+        else:
+            msg = 'F... L... U... B...'
+        await message.channel.send(msg)
       
-def dreamer(num):
+def dreamer(num, url):
     print('dreaming...\n')
-    url = message.attachments[0].url
     for x in range(num):
         r = requests.post("https://api.deepai.org/api/deepdream",data={'image':url,},headers={'api-key':API_Key})
         responce = str(r.json())
@@ -73,8 +76,6 @@ def dreamer(num):
         url = ''
         for i in range(x+14,len(responce)-2):
             url += responce[i]
-        await message.channel.send(url)
-        await message.channel.send('FℲ... L˥... U∩... Bq...')
-        return
-client.run(flub_token)
+        return url
+client.run(test_token)
 
